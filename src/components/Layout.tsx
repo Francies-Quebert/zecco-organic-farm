578130ea756821f101c119ab22b8b9052b0a1981
Francies Fernandes <quebert88@gmail.com>
2023-05-25 22:19:05 +0100

    feat: testing netify deployment without husky

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,66 +1,89 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Logo from './Logo';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 
 type LayoutProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 interface INavBar {
-    name: string
-    slug: string
-    childrenContentfulNavBarContentfulchildrenJsonNode: {
-        name: string
-        slug: string
-    }[]
+  name: string;
+  slug: string;
+  childrenContentfulNavBarContentfulchildrenJsonNode: {
+    name: string;
+    slug: string;
+  }[];
 }
 export default function Layout({ children }: LayoutProps) {
-
-
-    const { allContentfulNavBar }: {
-        allContentfulNavBar: {
-            nodes: INavBar[]
-        }
-    } = useStaticQuery(graphql`
-      query {
-        allContentfulNavBar {
-            nodes {
-              name
-              slug
-              childrenContentfulNavBarContentfulchildrenJsonNode {
-                name
-                slug
-              }
-            }
+  const {
+    allContentfulNavBar,
+  }: {
+    allContentfulNavBar: {
+      nodes: INavBar[];
+    };
+  } = useStaticQuery(graphql`
+    query {
+      allContentfulNavBar {
+        nodes {
+          name
+          slug
+          childrenContentfulNavBarContentfulchildrenJsonNode {
+            name
+            slug
           }
+        }
       }
-    `);
-    console.log(allContentfulNavBar.nodes, 'allContentfulNavBar')
+    }
+  `);
 
-    return (
-        <div className="">
-            <header className="bg-white flex items-center h-[80px] border border-transparent relative z-50">
-                <div className='w-[40%] mx-auto'><Logo /></div>
-                <div className='flex w-[60%] h-full'>
-
-                    {/* {allContentfulNavBar.nodes.map(({ name.childrenContentfulNavBarContentfulchildrenJsonNode }:INavBar)=>{
+  return (
+    <div className="">
+      <header className="bg-white flex items-center h-[80px] border border-transparent relative z-50">
+        <div className="w-[40%] mx-auto">
+          <Logo />
+        </div>
+        <div className="flex w-[60%] h-full">
+          {/* {allContentfulNavBar.nodes.map(({ name.childrenContentfulNavBarContentfulchildrenJsonNode }:INavBar)=>{
 
                     })} */}
-                    <ul className='flex items-center font-semibold space-x-[92px] mr-32'>
-                        {allContentfulNavBar.nodes.reverse().map(({ name, childrenContentfulNavBarContentfulchildrenJsonNode, slug }: INavBar, index) => {
-                            if (childrenContentfulNavBarContentfulchildrenJsonNode.length > 0) {
-                                return (<li className='group  menu-btn' key={index}>
-                                    {name} <span className="arrow"></span>
-                                    <ul className="bg-white absolute h-0 opacity-0 overflow-hidden group-hover:h-auto group-hover:opacity-100 shadow-lg w-44 pl-6 top-[79px] transition-opacity delay-75 ease-in-out left-[-37px]">
-                                        {childrenContentfulNavBarContentfulchildrenJsonNode.length > 0
-                                            && childrenContentfulNavBarContentfulchildrenJsonNode.map(({ name, slug }, i) => <li className='h-8' key={i}><Link to={slug} >{name}</Link></li>)}
-                                    </ul>
-                                </li>)
-                            } else {
-                                return <li className='menu-btn' key={index}><Link to={slug} >{name}</Link></li>
-                            }
-                        })}
-                        {/* <li className='group  menu-btn'>
+          <ul className="flex items-center font-semibold space-x-[92px] mr-32">
+            {allContentfulNavBar.nodes.map(
+              (
+                {
+                  name,
+                  childrenContentfulNavBarContentfulchildrenJsonNode,
+                }: INavBar,
+                index,
+              ) => {
+                if (
+                  childrenContentfulNavBarContentfulchildrenJsonNode.length > 0
+                ) {
+                  return (
+                    <li className="group  menu-btn" key={index}>
+                      {name} <span className="arrow"></span>
+                      <ul className="bg-white absolute h-0 opacity-0 overflow-hidden group-hover:h-auto group-hover:opacity-100 shadow-lg w-44 pl-6 top-[79px] transition-opacity delay-75 ease-in-out left-[-37px]">
+                        {childrenContentfulNavBarContentfulchildrenJsonNode.length >
+                          0 &&
+                          childrenContentfulNavBarContentfulchildrenJsonNode.map(
+                            ({ name }, i) => (
+                              <li className="h-8" key={i}>
+                                <Link to={`#`}>{name}</Link>
+                              </li>
+                            ),
+                          )}
+                      </ul>
+                    </li>
+                  );
+                } else {
+                  return (
+                    <li className="menu-btn" key={index}>
+                      <Link to={`#`}>{name}</Link>
+                    </li>
+                  );
+                }
+              },
+            )}
+            {/* <li className='group  menu-btn'>
                             Recipe box <span className="arrow"></span>
                             <ul className="bg-white absolute h-0 opacity-0 overflow-hidden group-hover:h-auto group-hover:opacity-100 shadow-lg w-44 pl-6 top-[79px] transition-opacity delay-75 ease-in-out left-[-37px]">
                                 <li className='h-8'><Link to="#" >Vegan</Link></li>
@@ -80,14 +103,16 @@ export default function Layout({ children }: LayoutProps) {
                         </li>
                         <li className='menu-btn'>About us</li>
                         <li className='menu-btn'>Visit us</li> */}
-                    </ul>
-                    <div className='space-x-11 items-center flex'>
-                        <button className=''>Log in</button>
-                        <button className='border border-accent h-[35px] w-[124px] font-thin text-[10px]'>CREATE ACCOUNT</button>
-                    </div>
-                </div>
-            </header>
-            <div>{children}</div>
+          </ul>
+          <div className="space-x-11 items-center flex">
+            <button className="">Log in</button>
+            <button className="border border-accent h-[35px] w-[124px] font-thin text-[10px]">
+              CREATE ACCOUNT
+            </button>
+          </div>
         </div>
-    )
+      </header>
+      <div>{children}</div>
+    </div>
+  );
 }
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,28 +1,41 @@
-
-import { IGatsbyImageData, getImage, GatsbyImage, ImageDataLike, StaticImage } from 'gatsby-plugin-image';
+import {
+  IGatsbyImageData,
+  getImage,
+  GatsbyImage,
+  ImageDataLike,
+  StaticImage,
+} from 'gatsby-plugin-image';
 
 export interface IRecipeCard {
-    image: ImageDataLike
-    name: string
-    price: string
+  image: ImageDataLike;
+  name: string;
+  price: string;
 }
 function RecipeCard({ image, name, price }: IRecipeCard) {
+  const recipeImage = getImage(image);
 
-
-    const recipeImage = getImage(image);
-
-    return (
-        <div className='flex card-recipe'>
-            <div className='w-[110px] h-[110px] relative'>
-                <GatsbyImage image={recipeImage as IGatsbyImageData} alt={name} className='rounded-full absolute w-full h-full -right-6 -top-4' />
-            </div>
-            <div className='z-30 mb-1 shadow-md w-[180px] h-[90px] pl-7 pr-5 font-Raleway text-sm flex flex-col justify-between py-2 '>
-                <div>{name}</div>
-                <div className='text-primary font-bold'>{price}</div>
-                <StaticImage className='absolute right-[53px] bottom-[29px]' src='../../static/images/cart.svg' alt='cart' width={12} height={12}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex card-recipe">
+      <div className="w-[110px] h-[110px] relative">
+        <GatsbyImage
+          image={recipeImage as IGatsbyImageData}
+          alt={name}
+          className="rounded-full absolute w-full h-full -right-6 -top-4"
+        />
+      </div>
+      <div className="z-30 mb-1 shadow-md w-[180px] h-[90px] pl-7 pr-5 font-Raleway text-sm flex flex-col justify-between py-2 ">
+        <div>{name}</div>
+        <div className="text-primary font-bold">{price}</div>
+        <StaticImage
+          className="relative right-[-100%] bottom-[-2px] w-[12px] h-[12px]"
+          src="../../static/images/cart.svg"
+          alt="cart"
+          width={12}
+          height={12}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default RecipeCard
+export default RecipeCard;
